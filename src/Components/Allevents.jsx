9a753fdef737ab6343efd0c1eea816479f717a44
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-09-23 00:31:23 +0530

    fetched the events

---
+++ src/Components/Allevents.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import './Allevents.css'
+
+const Allevents = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const response = await axios.get("https://cinenest-server.onrender.com/all-events");
+        setEvents(response.data);
+        console.log(response.data);
+      } catch (e) {
+        console.log("Error in fetching the events: " + e);
+      }
+    }
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <>
+       <section className="container">
+       <div className="eventcard">
+        {events.map((data) => (
+          <div  className="evcards" key={data._id}> {/* Wrap in a parent div */}
+            <img src={data.image} alt={data.title}   />
+            <h2>{data.title}</h2>
+            <p><i> {data.description}</i></p>
+            <p> <span style={{color:'red'}}> <b>Show time</b></span> {data.time}</p>
+            
+             <button>Book</button>
+
+          </div>
+        ))}
+      </div>
+       </section>
+    </>
+  );
+};
+
+export default Allevents;
